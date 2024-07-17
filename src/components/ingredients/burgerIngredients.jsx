3b961f7c143fc2a6fa308fc525392e2960cd3fe2
Bogdan Popov <114678431+TheJester32@users.noreply.github.com>
2024-07-17 17:02:21 +0300

    header done other parts stated

---
+++ src/components/ingredients/burgerIngredients.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { } from '@ya.praktikum/react-developer-burger-ui-components';
+import burgeringredientsStyles from './burgerIngredients.module.css';
+
+function BurgerIngredients() {
+    return (
+        <section>
+            <h1>Соберите бургер 2</h1>
+        </section>
+    );
+}
+
+export { BurgerIngredients };
